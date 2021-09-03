4ad8cde604ac60a93e9422ef593b359ee69fd4e5
Emory May <ec2-user@ip-172-31-41-190.us-east-2.compute.internal>
2021-09-03 16:22:48 +0000

    Add main.cs skeleton so that enbylib can compile standalone

---
+++ main.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnbyLib
+{
+    class Program
+    {
+        // exists to let EnbyLib compile on its own
+        static void Main(){
+            
+        }
+    }   
+}
